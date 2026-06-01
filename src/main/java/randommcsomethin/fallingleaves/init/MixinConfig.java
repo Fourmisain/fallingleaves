0a4fd37c46e6c57e59186234e818815725e2de2b
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2026-06-01 14:01:48 +0200

    do not apply GsonConfigSerializerMixin when bloom is loaded to prevent crash

--- src/main/java/randommcsomethin/fallingleaves/init/MixinConfig.java
+++ src/main/java/randommcsomethin/fallingleaves/init/MixinConfig.java
@@ -1,6 +1,7 @@
 package randommcsomethin.fallingleaves.init;
 
 import com.google.gson.JsonParseException;
+import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -40,10 +41,14 @@ public class MixinConfig implements IMixinConfigPlugin {
 		return true;
 	}
 
+	@Override
+	public List<String> getMixins() {
+		return FabricLoader.getInstance().isModLoaded("bloom") ? null : List.of("GsonConfigSerializerMixin");
+	}
+
 	@Override public void onLoad(String mixinPackage) {}
 	@Override public String getRefMapperConfig() { return null; }
 	@Override public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) { }
-	@Override public List<String> getMixins() { return null; }
 	@Override public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) { }
 	@Override public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) { }
 }
